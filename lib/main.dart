d2b5c04ee73eb6ca076cacfd5b49962f2431424a
eng-batol <batol.alashwak@gmail.com>
2025-01-19 11:50:24 +0300

    add the 2 new pages in the main to check if they worked or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,90 @@
+// import 'package:bkid_frontend/pages/Allowance_page.dart';
+// import 'package:bkid_frontend/pages/add_kid_page.dart';
+// import 'package:bkid_frontend/pages/allowance_dialogue.dart';
+// import 'package:bkid_frontend/pages/goals_page.dart';
+// import 'package:bkid_frontend/pages/home_page.dart';
+// import 'package:bkid_frontend/pages/restrictions_page.dart';
+// import 'package:bkid_frontend/pages/signIn_page.dart';
+// import 'package:bkid_frontend/pages/signUp_page.dart';
+// import 'package:bkid_frontend/pages/tasks_page.dart';
+// import 'package:bkid_frontend/pages/view_kidCard_page.dart';
+// import 'package:bkid_frontend/providers/auth_provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (_) => AuthProvider()),
+//         //Second provider
+//       ],
+//       child: MainApp(),
+//     ),
+//   );
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final GoRouter router = GoRouter(
+//       initialLocation: '/signin',
+//       routes: [
+//         GoRoute(
+//           path: '/signup',
+//           builder: (context, state) => SignupPage(),
+//         ),
+//         GoRoute(
+//           path: '/signin',
+//           builder: (context, state) => SigninPage(),
+//         ),
+//         GoRoute(
+//           path: '/home',
+//           builder: (context, state) => DashboardPage(),
+//         ),
+//         GoRoute(
+//           path: '/add-kid',
+//           builder: (context, state) => AddKidPage(),
+//         ),
+//         GoRoute(
+//           path: '/view-kid',
+//           builder: (context, state) => ViewKidCard(kid: {},),
+//         ),
+//         GoRoute(
+//           path: '/Restriction',
+//           builder: (context, state) => RestrictionsPage(),
+//         ),
+//         GoRoute(
+//           path: '/Goals',
+//           builder: (context, state) => GoalsPage(),
+//         ),
+//         GoRoute(
+//           path: '/Allowance',
+//           builder: (context, state) => AllowanceDialog(),
+//         ),
+//         GoRoute(
+//           path: '/Tasks',
+//           builder: (context, state) => TasksPage(),
+//         ),
+//       ],
+//     );
+//     return MaterialApp.router(
+//       debugShowCheckedModeBanner: false,
+//       routerConfig: router,
+//     );
+//   }
+// }
+
+import 'package:bkid_frontend/batolPages/AddGoalsDialog.dart';
+import 'package:bkid_frontend/batolPages/GoalsManagingScreenState.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+
+// Import your pages
 import 'package:bkid_frontend/pages/Allowance_page.dart';
 import 'package:bkid_frontend/pages/add_kid_page.dart';
 import 'package:bkid_frontend/pages/allowance_dialogue.dart';
@@ -9,16 +96,17 @@ import 'package:bkid_frontend/pages/signUp_page.dart';
 import 'package:bkid_frontend/pages/tasks_page.dart';
 import 'package:bkid_frontend/pages/view_kidCard_page.dart';
 import 'package:bkid_frontend/providers/auth_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
+
+// Import the new pages
+// import 'package:bkid_frontend/pages/goals_managing_screen.dart';
+// import 'package:bkid_frontend/pages/add_goals_dialog.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
-        //Second provider
+        // Add any additional providers here
       ],
       child: MainApp(),
     ),
@@ -31,7 +119,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final GoRouter router = GoRouter(
-      initialLocation: '/signin',
+      initialLocation: '/goals-managing',
       routes: [
         GoRoute(
           path: '/signup',
@@ -51,7 +139,9 @@ class MainApp extends StatelessWidget {
         ),
         GoRoute(
           path: '/view-kid',
-          builder: (context, state) => ViewKidCard(kid: {},),
+          builder: (context, state) => ViewKidCard(
+            kid: {},
+          ),
         ),
         GoRoute(
           path: '/Restriction',
@@ -69,8 +159,18 @@ class MainApp extends StatelessWidget {
           path: '/Tasks',
           builder: (context, state) => TasksPage(),
         ),
+        // Add routes for GoalsManagingScreen and AddGoalsDialog
+        GoRoute(
+          path: '/goals-managing',
+          builder: (context, state) => GoalsManagingScreen(),
+        ),
+        GoRoute(
+          path: '/add-goals-dialog',
+          builder: (context, state) => AddGoalsDialog(),
+        ),
       ],
     );
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
